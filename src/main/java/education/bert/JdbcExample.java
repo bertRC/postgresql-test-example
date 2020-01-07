105f956fb6fe82e65f32e7e66bd85407e63d1174
bertRC <sithalbert@ya.ru>
2020-01-07 23:16:27 +0300

    JdbcExample added

---
+++ src/main/java/education/bert/JdbcExample.java
@@ -0,0 +1,22 @@
+package education.bert;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcExample {
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://192.168.99.100:32768/postgres", "postgres", "")) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
