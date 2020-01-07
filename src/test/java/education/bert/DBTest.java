53d2d67c2b9496599d122af9ce549f400204488b
bertRC <sithalbert@ya.ru>
2020-01-07 23:16:43 +0300

    DBTest added

---
+++ src/test/java/education/bert/DBTest.java
@@ -0,0 +1,19 @@
+package education.bert;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class DBTest {
+    @Test
+    public void dbConnectionTest() throws SQLException {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://192.168.99.100:32768/postgres", "postgres", "");
+
+        assertNotNull(conn);
+    }
+}
